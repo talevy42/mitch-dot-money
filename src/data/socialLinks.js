dd3718fb37999502434dd05175d54a2a615ac102
eudaimonstro <SGCouch1@gmail.com>
2020-10-14 15:11:06 +0000

    trying to remove twitter icon

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,11 +1,4 @@
 // import facebookIcon from '../images/facebook.svg'
 // import twitterIcon from '../images/twitter.svg'
 
-export default [
-  //   {
-  //     label: 'Twitter',
-  //     url: 'https://twitter.com/ryudotmoney',
-  //     icon: twitterIcon
-  //   }
-  // { label: 'Facebook', url: 'https://facebook.com', icon: facebookIcon }
-]
+export default []
