6b3998f5d129f03c522955ac3d566be29ecde06b
eudaimonstro <SGCouch1@gmail.com>
2020-10-14 14:06:49 +0000

    Remove socials add GA

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,11 +1,11 @@
 // import facebookIcon from '../images/facebook.svg'
-import twitterIcon from '../images/twitter.svg'
+// import twitterIcon from '../images/twitter.svg'
 
 export default [
-  {
-    label: 'Twitter',
-    url: 'https://twitter.com/ryudotmoney',
-    icon: twitterIcon
-  }
+  //   {
+  //     label: 'Twitter',
+  //     url: 'https://twitter.com/ryudotmoney',
+  //     icon: twitterIcon
+  //   }
   // { label: 'Facebook', url: 'https://facebook.com', icon: facebookIcon }
 ]
